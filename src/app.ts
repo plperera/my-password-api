9f22d90de41b22aa290560b673b5a1cb60436aa8
Pedro Pereira <plp.leoncio@gmail.com>
2023-08-25 10:15:16 -0300

    fix: erro de build + erro no tsconfig resolvido

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import { loadEnv, connectDb, disconnectDB } from "@/config";
 import cors from "cors";
 import express, { Express } from "express";
+
 import "reflect-metadata";
 import "express-async-errors";
+
+loadEnv();
+
 import { authRouter } from "./routers/auth-router";
 import { handleSaveRouter } from "./routers/handleSave-router";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger.json";
-loadEnv();
 
 const app = express();
 app
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+require('module-alias/register');
+
 import app, { init } from "@/app";
 
 const port = +process.env.PORT || 4000;
