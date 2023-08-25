5faa7ec7dc196e57bfe6f13c3dbb4d7a1ad0be7b
Pedro Pereira <plp.leoncio@gmail.com>
2023-08-25 10:07:15 -0300

    feat: api documentada com swagger

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,15 @@ import "reflect-metadata";
 import "express-async-errors";
 import { authRouter } from "./routers/auth-router";
 import { handleSaveRouter } from "./routers/handleSave-router";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 loadEnv();
 
 const app = express();
 app
   .use(cors())
   .use(express.json())
+  .use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
   .use("/auth", authRouter)
   .use("/item", handleSaveRouter)
   .get("/health", (_req, res) => res.send("OK!"))
